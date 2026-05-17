fdebab626c0e1008c7063e0e8fa0ed6b8de8fd37
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-17 12:40:56 +0300

    Parsing User Data With Express with middleware function

--- S18_ExpressJS_Server-side_Code/app.js
+++ S18_ExpressJS_Server-side_Code/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const appexpress = express();
 
+appexpress.use(express.urlencoded({ extended: false })); //urlencode->middleware function
+
 appexpress.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // localhost:3000/currenttime
 
 appexpress.get("/", function (req, res) {
-  res.send("<h1>Hi Hommies, I'm Alien</h1>");
+  res.send(
+    '<form action="/store-user" method="POST"><lable>Your Name</lable><input type="text" name="username"/><button>Submit</button></form>',
+  );
 }); // localhost:3000/
 
+appexpress.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h1>Username stored!</h1>");
+});
 appexpress.listen(3000);
