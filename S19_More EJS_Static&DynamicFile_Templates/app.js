a09b28cc1cbb0cb9c0004a2df8a9e34c9f76360b
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-18 14:15:28 +0300

    Serving HTML Files With Node & Express

---
+++ S19_More EJS_Static&DynamicFile_Templates/app.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  const filePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(filePath);
+});
+
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(filePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const filePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(filePath);
+});
+
+app.get("/about", function (req, res) {
+  const filePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(filePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const filePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(filePath);
+});
+
+app.listen(3000);
