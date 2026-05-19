2fcb4e6f8cb243a168f190be0ce05e09cca710aa
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-19 14:04:23 +0300

    Parsing Form Data & Redirecting POST Requests

--- S19_More EJS_Static&DynamicFile_Templates/app.js
+++ S19_More EJS_Static&DynamicFile_Templates/app.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const filePath = path.join(__dirname, "views", "index.html");
@@ -18,6 +20,18 @@ app.get("/restaurants", function (req, res) {
 app.get("/recommend", function (req, res) {
   const filePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(filePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurants = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurants);
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/about", function (req, res) {
