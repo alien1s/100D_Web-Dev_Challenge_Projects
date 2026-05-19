648558979b1cf3c971f20d8e1ae07128508d2285
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-19 14:41:35 +0300

    Adding the EJS Template Engine

--- S19_More EJS_Static&DynamicFile_Templates/app.js
+++ S19_More EJS_Static&DynamicFile_Templates/app.js
@@ -4,22 +4,22 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  const filePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(filePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(filePath);
+  res.render("restaurants");
 });
 
 app.get("/recommend", function (req, res) {
-  const filePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(filePath);
+  res.render("recommend");
 });
 
 app.post("/recommend", function (req, res) {
@@ -35,13 +35,11 @@ app.post("/recommend", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  const filePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(filePath);
+  res.render("about");
 });
 
 app.get("/confirm", function (req, res) {
-  const filePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(filePath);
+  res.render("confirm");
 });
 
 app.listen(3000);
