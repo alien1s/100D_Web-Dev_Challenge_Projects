5cd04a29002fdad7d9c24858fe5841a46154e21b
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-19 09:59:53 +0300

    Serving the  Static Files (CSS & JavaScript) through public folder by middleware

--- S19_More EJS_Static&DynamicFile_Templates/app.js
+++ S19_More EJS_Static&DynamicFile_Templates/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/", function (req, res) {
   const filePath = path.join(__dirname, "views", "index.html");
   res.sendFile(filePath);
