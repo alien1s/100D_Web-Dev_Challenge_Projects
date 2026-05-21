0442ce53f93790473cc7bfbfad04e68eac2c568e
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-21 12:37:13 +0300

    change the project folder name

---
+++ S19&20_More EJS_Static&DynamicFile_Templates&Handling Dynamic Routes (URLs), Errors & Manag. Bigger Express Projects/app.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", { restaurantsNumber:storedRestaurants.length, restaurants:storedRestaurants});
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurants = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurants);
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.listen(3000);
