040d361f489a1513a77189e7300a15088b1b15b0
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-05-31 00:16:23 +0300

    Use  Rest Parameters & The Spread Operator

--- S21_More_Advanced_JS_Concepts/functions.js
+++ S21_More_Advanced_JS_Concepts/functions.js
@@ -4,3 +4,19 @@ function greatinUser(greatinPrefix, userName = "user") {
 
 greatinUser("Hi", "Max");
 greatinUser("Hello");
+
+function sumUp(...numbers) {
+  let result = 0;
+
+  for (const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+console.log(sumUp(1, 5, 10, 11, 20, 31));
+
+const inputNumbers = [1, 5, 10, 11, 20, 31];
+
+console.log(sumUp(...inputNumbers));
