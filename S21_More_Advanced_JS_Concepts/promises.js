5c82504736ec41a62ba099f8b0120f5af8993f5e
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-06-04 22:55:05 +0300

    Asynchronous Code with Error Handling

--- S21_More_Advanced_JS_Concepts/promises.js
+++ S21_More_Advanced_JS_Concepts/promises.js
@@ -1,15 +1,35 @@
 const fs = require("fs/promises");
 
-function readFile() {
-  fs.readFile("data.txt")
+// function readFile() {
+//   fs.readFile("data.txt")
+//     .then(function (fileData) {
+//       console.log("file parsing done!");
+//       console.log(fileData.toString());
+//       //return anotherAsyncOperation;
+//     })
+//     .then(function () {})
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+
+//   console.log("Hi there!");
+// }
+
+// readFile();
+
+function readFile2() {
+  fs.readFile("dataa.txt")
     .then(function (fileData) {
       console.log("file parsing done!");
       console.log(fileData.toString());
       //return anotherAsyncOperation;
     })
-    .then(function () {});
+    .then(function () {})
+    .catch(function (error) {
+      console.log(error);
+    });
 
   console.log("Hi there!");
 }
 
-readFile();
+readFile2();
