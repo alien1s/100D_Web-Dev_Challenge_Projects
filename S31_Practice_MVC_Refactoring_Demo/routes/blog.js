7f63fcd9e1e8e1b69f860838a4328e5126d9c05c
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-07-27 18:07:11 +0300

    adding update and delete functionalitiy to Post model

--- S31_Practice_MVC_Refactoring_Demo/routes/blog.js
+++ S31_Practice_MVC_Refactoring_Demo/routes/blog.js
@@ -1,11 +1,13 @@
 const express = require("express");
+
 const mongodb = require("mongodb");
 
 const db = require("../data/database");
 
+const ObjectId = mongodb.ObjectId;
+
 const Post = require("../models/post");
 
-const ObjectId = mongodb.ObjectId;
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -95,7 +97,6 @@ router.get("/posts/:id/edit", async function (req, res) {
 router.post("/posts/:id/edit", async function (req, res) {
   const enteredTitle = req.body.title;
   const enteredContent = req.body.content;
-  const postId = new ObjectId(req.params.id);
 
   if (
     !enteredTitle ||
@@ -114,20 +115,15 @@ router.post("/posts/:id/edit", async function (req, res) {
     return;
   }
 
-  await db
-    .getDb()
-    .collection("posts")
-    .updateOne(
-      { _id: postId },
-      { $set: { title: enteredTitle, content: enteredContent } },
-    );
+  const postUpdated = new Post(enteredTitle, enteredContent, req.params.id);
+  await postUpdated.save();
 
   res.redirect("/admin");
 });
 
 router.post("/posts/:id/delete", async function (req, res) {
-  const postId = new ObjectId(req.params.id);
-  await db.getDb().collection("posts").deleteOne({ _id: postId });
+  const selectedPost = new Post(null, null, req.params.id);
+  await selectedPost.delete();
 
   res.redirect("/admin");
 });
