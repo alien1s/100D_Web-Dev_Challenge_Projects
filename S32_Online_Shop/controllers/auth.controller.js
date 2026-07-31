f7d1825686597a87e34cf9931701be65c8105ecb
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-07-31 07:31:57 +0300

    Populating & Rendering The First View signp page

--- S32_Online_Shop/controllers/auth.controller.js
+++ S32_Online_Shop/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 
 function getSignup(req, res) {
   //handling logic
+  res.render("customer_views/auth_views/signup");
 }
 
 function getLogin(req, res) {
--- S32_Online_Shop/routes/auth.routes.js
+++ S32_Online_Shop/routes/auth.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const authController = require("../controllers/auth.controller");
 
-const router = express.Router;
+const router = express.Router();
 
 //.....config
-router.get("/sginup", authController.getSignup);
+router.get("/signup", authController.getSignup);
 router.get("/login", authController.getLogin);
 
 module.exports = router;
