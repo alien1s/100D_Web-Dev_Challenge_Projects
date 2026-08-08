67ae51db5aee63b1d86b5fb8fd87ce7ed6979d42
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-08-08 16:39:21 +0300

    Outputting Products For Customers

--- S32_Online_Shop/controllers/products.controller.js
+++ S32_Online_Shop/controllers/products.controller.js
@@ -1,6 +1,15 @@
-function getProductsList(req, res) {
-  //handling logic
-  res.render("customer_views/products_views/all-products");
+const Product = require("../models/product.model");
+
+async function getProductsList(req, res, next) {
+  try {
+    const productsDocs = await Product.fetchAll();
+    res.render("customer_views/products_views/all-products", {
+      products: productsDocs,
+    });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
 module.exports = {
