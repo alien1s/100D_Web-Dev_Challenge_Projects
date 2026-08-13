fc7b9fda96c615e84d2d97f72d390f1465f0f5ef
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-08-13 18:16:48 +0300

    Displaying Orders For Admin

--- S32_Online_Shop/controllers/admin.controller.js
+++ S32_Online_Shop/controllers/admin.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product.model");
+const Order = require("../models/order.model");
 
 async function getProducts(req, res, next) {
   try {
@@ -71,7 +72,33 @@ async function deleteProduct(req, res, next) {
   res.json({ message: "Product was deleted!" });
 }
 
+async function getOrders(req, res, next) {
+  try {
+    const orders = await Order.findAll();
+    res.render("admin_views/orders/orders-admin", {
+      orders: orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
 
+async function updateOrder(req, res, next) {
+  const orderId = req.params.id;
+  const newStatus = req.body.newStatus;
+
+  try {
+    const order = await Order.findByOrderId(orderId);
+
+    order.status = newStatus;
+
+    await order.save();
+
+    res.json({ message: "Order updated", newStatus: newStatus });
+  } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = {
   getProducts: getProducts,
@@ -80,4 +107,6 @@ module.exports = {
   getUpdateProduct: getUpdateProduct,
   updateProduct: updateProduct,
   deleteProduct: deleteProduct,
+  getOrders: getOrders,
+  updateOrder: updateOrder,
 };
