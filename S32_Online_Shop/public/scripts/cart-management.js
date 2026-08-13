379b40a2e37be9d00bd3ee89bafcfda099bd9d5f
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-08-13 19:03:49 +0300

    update all badge bug fix

--- S32_Online_Shop/public/scripts/cart-management.js
+++ S32_Online_Shop/public/scripts/cart-management.js
@@ -1,7 +1,7 @@
 const addToCartButtonElement = document.querySelector(
   "#product-details button",
 );
-const cartBadgeElement = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid;
@@ -34,7 +34,9 @@ async function addToCart() {
   const newTotalQuantity = responseData.totalItemsNum;
 
   if (cartBadgeElement) {
-    cartBadgeElement.textContent = newTotalQuantity;
+    for (const cartBadgeElement of cartBadgeElements) {
+      cartBadgeElement.textContent = newTotalQuantity;
+    }
   }
 }
 
