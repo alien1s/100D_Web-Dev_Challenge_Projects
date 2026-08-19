4a116fca19ba2bb22b2e08d7020f448b714b8192
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-08-19 13:50:10 +0300

    Move Stripe key to env var

--- S32_Online_Shop/controllers/order.controller.js
+++ S32_Online_Shop/controllers/order.controller.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.env.SECRET_STRIPE_TOKEN);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const User = require("../models/user.model");
 const Order = require("../models/order.model");
