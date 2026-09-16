0667a14fdf570b0d7fe268746c9edfc1be7a1112
Saif-Eldin A Abdelrahman <saif.at.miracle@gmail.com>
2026-09-16 15:42:54 +0300

    Listening To Events & Updating Data + the DOM & using Two-Way-Binding

--- S36_working_with_frontend_framework_Vue/decoupled-frontend-vue/scripts/app.vue.js
+++ S36_working_with_frontend_framework_Vue/decoupled-frontend-vue/scripts/app.vue.js
@@ -1,10 +1,18 @@
 const TodoApp = {
-    data(){
-        return {
-            newTodo: 'learn Vue.js'
-        }
-    }
-}
+  data() {
+    return {
+      newTodo: "learn Vue.js",
+      enteredTodoText: "",
+    };
+  },
 
-Vue.createApp(TodoApp).mount('#todos-app')
+  methods: {
+    saveTodo(event) {
+      event.preventDefault();
+      this.newTodo = this.enteredTodoText;
+      this.enteredTodoText = "";
+    },
+  },
+};
 
+Vue.createApp(TodoApp).mount("#todos-app");
